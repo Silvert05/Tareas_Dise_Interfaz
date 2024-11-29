2aa0d755ff3806c63bb8025aa37c34494c261332
Josselyn Isabel Toasa Tutalcha <lpa.carrillo@yavirac.edu.ec>
2024-11-28 21:31:50 -0500

    deberes del 1 al 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Grid1Component } from './components/grid-1/grid-1.component';
+import { Grid2Component } from './components/grid-2/grid-2.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'grid-1', component: Grid1Component },
+  { path: 'grid-2', component: Grid2Component },
+  { path: 'cards', component: CardsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
